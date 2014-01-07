94a0a7a99958d169cc518a4e4a1d8f3227ae4042
Kordan <kordan@realtek.com>
2014-01-07 10:52:21 +0800

    Add daily.pl to generate the weekly report.

--- RevisionBuild.pl
+++ RevisionBuild.pl
@@ -1,4 +1,7 @@
 use File::Copy;
+use Getopt::Mixed;
+
+BEGIN { $| = 1 }
 
 $num_args = $#ARGV + 1;
 if ($num_args < 1) {
@@ -33,15 +36,42 @@ while ( ! eof(FH) ) {
 foreach  my $v (@ARGV) {
     while ((my $version, my $hash) = each(%log)){
         if ($v =~/$version/i) {
-            print "git co $hash -b $version\r\n";
+            print "[Checking out $version...]\n\n";
 
+            Getopt::Mixed::getOptions('t=s i=s help ?>help');
+
+            if ($opt_t =~ "" or $opt_i =~ "") {
+                print "opt_i=$opt_i, opt_t=$opt_t\n";
+                print "revbuild -t [86|64] -i [e|u|s]\n";
+                last;
+            }
+            print "git co -f $hash -b $version\r\n";
+
+            $i = $opt_i ? lc $opt_i : "e";
+            $t = $opt_t ? $opt_t : "86";
+
+            if ($t =~ "86") {
+                $o = "WXP"; $d = "WinXP_2K";
+            } elsif ($t =~"64") {
+                $o = "AMD64 WNET"; $d = "X64";
+            } else {
+                last;
+            }
+
+            print "$o $d $i";
             chdir "C:\\WLAN\\Trunk.git" or die "$!";
-            system "git checkout $hash -b $version";
+            if (-e "C:/WLAN/Trunk.git/RollBack/$version/$d/rtwlan$i\mp.sys") {
+                print "Driver already compiled, skipped!";
+                next;
+            }
+            
 
-            system "start C:/MassProductionKit/Utility/setenv.bat C:/WINDDK/3790~1.183 chk AMD64 WNET USB";
-            $userinput = <STDIN>;
+            system "git checkout -f $hash -b $version";
+            system("cmd.exe", "/c", "C:/WINDDK/3790~1.183/bin/setenv.bat C:/WINDDK/3790~1.183 chk $o && cd C:/WLAN/Trunk.git && mpbuild c$i");
             mkdir "C:/WLAN/Trunk.git/RollBack/$version"; 
-            copy("C:/WLAN/Trunk.git/RTLWlanU_WindowsDriver_/X64/rtwlanump.sys","C:/WLAN/Trunk.git/RollBack/$version/rtwlanump.sys") or die "Copy failed: $!";
+            mkdir "C:/WLAN/Trunk.git/RollBack/$version/$d"; 
+            #copy("C:/WLAN/Trunk.git/RTLWlan$i\_WindowsDriver_/$d/rtwlan$i\mp.sys","C:/WLAN/Trunk.git/RollBack/$version/$d/rtwlan$i\mp.sys") or die "Copy failed: $!";
+            copy("C:/WLAN/Trunk.git/RTLWlan$i\_WindowsDriver_/$d/rtwlan$i\mp.sys","C:/WLAN/Trunk.git/RollBack/$version/$d/rtwlan$i\mp.sys");
         }
     }
 }
---
+++ daily.pl
@@ -0,0 +1,61 @@
+﻿#use encoding 'big5', STDIN => 'big5', STDOUT => 'big5';
+
+use Encode qw/from_to/;
+
+use File::Copy; 
+use Getopt::Mixed;
+use Encode;
+
+binmode(STDOUT, ":utf8");
+
+BEGIN { $| = 1 }
+
+$num_args = $#ARGV + 1;
+if ($num_args < 1) {
+  print "\nUsage: daily.pl SRC_FILE DST_FILE\n";
+  exit;
+}
+
+$daily_file = $ARGV[0];
+$output = $ARGV[1];
+%daily = ();
+
+if ( ! open(FH, "<", $daily_file) ) {
+    system("cd C:/WLAN/Trunk.git && git log > ./GitLog.txt");
+    open(FH, "<", $daily_file) or die "Can't open $daily_file: $!";
+}
+
+
+open(OUTFILE, "+>", $output) or die "Can't open out.txt $!";
+
+$ic = "";
+$report = "";
+while ( ! eof(FH) ) {
+
+    defined( $_ = <FH> ) or die "readline failed for $daily_file: $!";
+
+    if ($_ =~ m/\[(\w+)\] (.*)/) {
+        $ic = $1;
+        $daily{$ic} .= $& . "\n";
+    } elsif ($_ =~ m/^[^\n\w](.*)/) {
+        $daily{$ic} .= $1 . "\n";
+    }
+}
+
+while ((my $ic, my $report) = each(%daily)){
+    #$ic = decode("utf8", $ic);
+    #Encode::_utf8_on($ic);
+    #Encode::_utf8_on($report);
+    #$report = encode('utf8', $report);
+    #print OUTFILE "\Q$ic\E";
+    #print OUTFILE "\Q$report\E";
+    print  OUTFILE "------$ic------\n\n";
+    print  OUTFILE "$report\n";
+    #if (Encode::is_utf8($ic)) {
+    #    print "hi";
+    #} else {
+    #    print "no";
+    #}
+    #print encode("big5", decode("utf8", $ic));
+    #print encode("big5", decode("utf8", $report));
+}
